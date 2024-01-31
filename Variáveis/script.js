048b43fb07bdf7709f6126368fe67d85a38b0bad
Estelle Moreira <157535316+EstelleMoreira@users.noreply.github.com>
2024-01-31 16:07:21 +0000

    Add files via upload

---
+++ Variáveis/script.js
@@ -0,0 +1,13 @@
+
+// esta variável é do tipo String
+var cidadeAtual = 'Aveiro';
+
+// esta variável é do tipo Number
+var numeroIrmaos = 1;
+
+// esta variável é do tipo Boolean
+var cartaDeCondução = true;
+
+console.log(cidadeAtual);
+console.log(numeroIrmaos);
+console.log(cartaDeCondução)
